b972c2800e279038cac63ac1e6dc920f15dbf245
sewitghebreselase <sewitghebreselase@gmail.com>
2024-04-05 00:13:35 -0500

    basics of spring and  spring boot, component based architecture

---
+++ Assignment3/elibrary/src/main/java/miu/edu/cs489/elibrary/controller/HomePageController.java
@@ -0,0 +1,11 @@
+package miu.edu.cs489.elibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomePageController {
+    @GetMapping(value = {"/", "/elibrary", "/elibrary/home"})
+    public String displayHomePage() {
+        return "home/index";
+    }
+}
